c657184e6e20869dd4ca14e0858810be0937f35d
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-06-17 21:48:40 +0500

    add group lambdas and entity

---
+++ stacks/lambdas/group/create-group-lambda.ts
@@ -0,0 +1,47 @@
+import { Logger } from '@aws-lambda-powertools/logger';
+import { AppSyncResolverHandler } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+import { Group, MutationCreateGroupArgs } from '../../../appsync';
+import { uuid } from '../../utils';
+import GroupEntity from './group-entity';
+
+const logger = new Logger({ serviceName: 'create-group-lambda' });
+
+export const handler: AppSyncResolverHandler<
+  MutationCreateGroupArgs,
+  Group
+> = async (event) => {
+  const documentClient = new DynamoDB.DocumentClient();
+
+  let tableName = process.env.GroupChat_DB;
+
+  const createdOn = Date.now();
+
+  const id: string = uuid();
+
+  if (tableName === undefined) {
+    logger.error(`Couldn't get the table name`);
+    tableName = 'groupChatTable';
+  }
+
+  const groupInput: GroupEntity = new GroupEntity({
+    id: id,
+    ...event.arguments.input,
+    createdOn,
+  });
+
+  logger.info(`group input info", ${JSON.stringify(groupInput)}`);
+  const params = {
+    TableName: tableName,
+    Item: groupInput.toItem(),
+  };
+
+  try {
+    await documentClient.put(params).promise();
+    return groupInput.graphQlReturn();
+  } catch (error: any) {
+    logger.error(`an error occured while creating user ${error}`);
+    throw error;
+  }
+};
+
