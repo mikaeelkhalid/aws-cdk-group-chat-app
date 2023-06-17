7f73c6603a328fe8c06a2bd6f9f4edc02c4b6d0d
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-06-17 12:40:08 +0500

    Initial commit

---
+++ test/aws-cdk-group-chat-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkGroupChatApp from '../lib/aws-cdk-group-chat-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-group-chat-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkGroupChatApp.AwsCdkGroupChatAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
